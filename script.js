fb1d2e239e6c5ff9c3925dd457c7135b8d48604f
vildanerustemi-tech <vildanerustemi@gmail.com>
2025-12-26 19:47:58 +0100

    Add welcome alert and info button functionality

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("Mirë se vini në Shtatzania ime 💗");
+document.getElementById("infoBtn").addEventListener("click", function () {
+  alert("Kujdes: Informacionet në këtë faqe nuk zëvendësojnë këshillat e mjekut.");
+});
